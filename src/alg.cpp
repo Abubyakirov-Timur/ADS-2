0058b161a263433acab82dfcdfc54fe8fce1d9d0
Abubyakirov-Timur <99200506+Abubyakirov-Timur@users.noreply.github.com>
2022-03-07 01:29:48 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,12 +4,12 @@
 
 
 double pown(double value, uint16_t n) {
-   if (n == 1) {
-     return value;
-   } else if (n == 0) {
-     return 1;
-   }
-   return value * pown(value, --n);
+  if (n == 1) {
+    return value;
+  } else if (n == 0) {
+    return 1;
+  }
+  return value * pown(value, --n);
 }
 
 uint64_t fact(uint16_t n) {
@@ -32,11 +32,11 @@ double expn(double x, uint16_t count) {
 }
 
 double sinn(double x, uint16_t count) {
- double sum = 0;
- for (uint16_t i = 1, n = 1; i <= count; ++i, ++n) {
-   sum += pown(-1, n - 1) * calcItem(x, 2 * n - 1);
- }
- return sum;
+  double sum = 0;
+  for (uint16_t i = 1, n = 1; i <= count; ++i, ++n) {
+    sum += pown(-1, n - 1) * calcItem(x, 2 * n - 1);
+  }
+  return sum;
 }
 
 double cosn(double x, uint16_t count) {
